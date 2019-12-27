4d09924e019177c0b4fe3725a2c8f020789371e8
Mauricio P <mauriciopdvg@gmail.com>
2019-12-27 17:59:24 -0300

    Add SessionsController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,17 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  # describe "POST #create" #do
-    it "returns http ok" #do
-  #     get :create
-  #     expect(response).to have_http_status(:ok)
-  #   end
-  # end
+  describe 'GET #new' do
+    it 'returns http ok' do
+      get :new
 
-  # describe "DELETE #destroy" #do
-    it "returns http ok" #do
-  #     get :destroy
-  #     expect(response).to have_http_status(:ok)
-  #   end
-  # end
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST #create" do
+    let!(:user) { create(:user) }
+
+    subject { 
+      post :create, params: {
+        session: {
+          username: user.username,
+          password: user.password
+        }
+      }
+    }
+
+    it 'has session[:user_id]' do
+      subject
+
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it "returns the flash message" do
+      subject
+
+      expect(flash[:notice]).to match(/You have successfully logged in./)
+    end
+
+    it 'redirects to root_path' do
+      subject
+
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe "DELETE #destroy" do
+    let!(:user) { create(:user) }
+    let!(:login) { 
+      post :create, params: {
+        session: {
+          username: user.username,
+          password: user.password
+        }
+      }
+    }
+
+    subject { delete :destroy, params: { id: user.id } }
+
+    it 'has session[:user_id] = nil' do
+      subject
+
+      expect(session[:user_id]).to eq(nil)
+    end
+
+    it "has flash message" do
+      subject
+
+      expect(flash[:notice]).to match(/You have logged out./)
+    end
+
+    it 'redirects to login page' do
+      subject
+
+      expect(response).to redirect_to(login_path)
+    end
+  end
 end
