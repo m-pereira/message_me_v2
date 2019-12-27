b26fa574ebd4993471dad373f46f13b7d24e0977
Mauricio P <mauriciopdvg@gmail.com>
2019-12-27 19:28:42 -0300

    Add Message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 
+  has_many :messages
+
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe User, type: :model do
     it { create(:user); is_expected.to validate_uniqueness_of(:username) }
     it { is_expected.to validate_presence_of(:password) }
   end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:messages) }
+  end
 end
