1eb07c9e656bb19834f1ea1ebe0c1ca23862e5a6
Mauricio Pereira <mauriciopdvg@gmail.com>
2019-12-27 16:05:48 -0300

    Add User model (#1)

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:username) }
+    it { create(:user); is_expected.to validate_uniqueness_of(:username) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
+end
