575aeaa0fed52083b7d8c76500cb39b4ecbd3ef4
Mauricio Pereira <mauriciopdvg@gmail.com>
2019-12-27 18:01:43 -0300

    Add SessionsController (#5)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_id?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def require_user
+    redirect_to root_path, alert: 'You must be logged in to perform that.' unless logged_in?
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,20 @@
 class SessionsController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    user = User.find_by(username: params[:session][:username])
 
-  def destroy; end
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'You have successfully logged in.'
+    else
+      flash.now[:alert] = 'There was something wrong with your login information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: 'You have logged out.'
+  end
 end
