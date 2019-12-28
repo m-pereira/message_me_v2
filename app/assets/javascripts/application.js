31a01b1cae6c6d8d057980c4c82e5e02588a38de
Mauricio P <mauriciopdvg@gmail.com>
2019-12-28 00:46:10 -0300

    Add scroll bottom functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,4 +22,11 @@ $(document).on('turbolinks:load', function() {
   $('.message .close').on('click', function() {
       $(this).closest('.message').transition('fade');
   });
+  scroll_bottom();
 })
+
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
